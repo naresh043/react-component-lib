e389d9aa355e5693d69e9b2f35a0276e2a50457a
NareshSanjeev <naresh732003@gmail.com>
2026-01-03 13:02:17 +0530

    Change: setUp the package.json and vite config

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [react()],
   build: {
+    cssCodeSplit: true,
     lib: {
       entry: resolve(__dirname, "src/index.js"),
       name: "MyLib",
