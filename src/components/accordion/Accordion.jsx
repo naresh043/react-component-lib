9df3a9ea771b1a08a956c8a3d60261aca050cf58
NareshSanjeev <naresh732003@gmail.com>
2026-01-03 11:02:58 +0530

    change: remove the logs in accordion component

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,22 +1,16 @@
 import React, { useState } from "react";
 
-function Accordion({ children}) {
+function Accordion({ children }) {
   const [active, setActive] = useState(null);
-//   console.log(children);
   return (
-    <>
-      {/* <div className="accordion" id="accordionExample">
-        {children}
-      </div> */}
-      <div className="accordion" id="accordionExample">
-        {React.Children.map(children, (child, index) =>
-          React.cloneElement(child, {
-            isOpen: active == index,
-            onToggle: () => setActive(index),
-          })
-        )}
-      </div>
-    </>
+    <div className="accordion" id="accordionExample">
+      {React.Children.map(children, (child, index) =>
+        React.cloneElement(child, {
+          isOpen: active == index,
+          onToggle: () => setActive(index),
+        })
+      )}
+    </div>
   );
 }
 
